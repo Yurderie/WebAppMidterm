54ca6760335d4eb8bbbf1076c772adc071223d25
Yurderie <emreyurderi@gmail.com>
2023-03-01 13:11:34 -0500

    second commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://emre:E1m9r9e1#@cluster0.3uahiq7.mongodb.net/?retryWrites=true&w=majority"
 };
